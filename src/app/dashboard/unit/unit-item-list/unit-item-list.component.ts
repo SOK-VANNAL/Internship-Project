ff2e7ed3aa31d8121c319ccf039bce106c98dca3
unknown <you@example.com>
2021-07-24 08:37:45 +0700

    third commit: Pagination & sort data

--- src/app/dashboard/unit/unit-item-list/unit-item-list.component.ts
+++ src/app/dashboard/unit/unit-item-list/unit-item-list.component.ts
@@ -16,42 +16,72 @@ interface item{
 export class UnitItemListComponent implements OnInit {
   @Input() parentSubject:Subject<any> | undefined;
   listOfProducts:any=[];
-  index:number=0;
+  param:any={};
+  order:number=0;
+  loading = true;
+  sort:string = "";
   constructor(private productService: ProductService) { }
 
-  sortFn = (a:item, b:item) => a.name.localeCompare(b.name);
+  sortData(sort:any){
+    if(sort == "ascend"){
+      this.sort = 'name'
+    }
+    else if(sort == "descend"){
+      this.sort = 'name-'
+    }
+    else{
+      this.sort =''
+    }
 
-  getUnitItem(){
-    this.productService.getUnitItem().subscribe(
-      (result:any)=>{
-        this.listOfProducts = result.results
-      },
-      (err)=>{
-        console.log(err)
-      }
-    )
+    this.getUnitItem(1,10);
   }
 
+  // sortFn = (a:item, b:item) => a.name.localeCompare(b.name);
+
+  // getUnitItem(){
+  //   this.productService.getUnitItem().subscribe(
+  //     (result:any)=>{
+  //       this.listOfProducts = result.results
+  //     },
+  //     (err)=>{
+  //       console.log(err)
+  //     }
+  //   )
+  // }
+
   updateProduct(){
-    this.getUnitItem();
+    this.getUnitItem(1, 10);
+    this.order = 0
   }
 
   ngOnInit(): void {
-    this.getUnitItem();
+    // this.getUnitItem();
+    this.getUnitItem(1, 10);
     this.parentSubject?.subscribe(event=>{
-      this.getUnitItem();
+      this.getUnitItem(1, 10);
+      this.order = 0
     });
-    this.sortData();
+    
   }
 
-  onPageIndexChange(data:any){
-    this.index = (data-1)*10
+  onPageIndexChange(pageIndex:any){
+    this.order = (pageIndex-1)*10
+    this.param.pageIndex = pageIndex;
+    this.getUnitItem(this.param.pageIndex, 10);
   }
 
-  sortData(){
-    this.productService.sortUnitItem(1, 25, 'name').subscribe(
-      (result)=>{
-        console.log(result)
+  getUnitItem(pageIndex:number, pageSize:number){
+    this.loading = true
+    this.sort ? this.sort : ''
+    this.productService.getUnitItem(pageIndex, pageSize, this.sort).subscribe(
+      (result:any)=>{
+        this.listOfProducts = result.results;
+        this.param = result.param
+        // console.log(this.param)
+        this.loading = false;
+      },
+      (err)=>{
+        console.log(err)
       }
     )
   }
--- src/app/service/product-service/product.service.ts
+++ src/app/service/product-service/product.service.ts
@@ -8,16 +8,15 @@ export class ProductService {
 
   constructor(private http: HttpClient) { }
 
-  getUnitItem(){
-    return this.http.get('https://sale-api.sgx.bz/api/Unit?pageIndex=1&pageSize=25&sorts=&filters=');
-  };
+  // getUnitItem(){
+  //   return this.http.get('https://sale-api.sgx.bz/api/Unit?pageIndex=1&pageSize=25&sorts=&filters=');
+  // };
 
   updateUnitItem(name:string, note:string|null, id:string){
     return this.http.put('https://sale-api.sgx.bz/api/Unit/5', {id, name, note});
   };
 
   deleteUnititem(id:string, name:string, note:string){
-    console.log(id, name, note)
     const  options = {
       headers: new HttpHeaders({
         'Content-Type': 'application/json',
@@ -35,7 +34,7 @@ export class ProductService {
     return this.http.post("https://sale-api.sgx.bz/api/Unit", {name, note});
   };
 
-  sortUnitItem(pageNumber:number, pageSize:number, sort:string){
+  getUnitItem(pageNumber:number, pageSize:number, sort:string){
     return this.http.get(`https://sale-api.sgx.bz/api/unit?pageIndex=${pageNumber}&pageSize=${pageSize}&Sorts=${sort}&filters=`);
   }
 }
